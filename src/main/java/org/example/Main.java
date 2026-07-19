86aa68292da2b3c6814660c2032ec2c75bc8934a
reshetov.uriy@gmail.com <reshetov.uriy@gmail.com>
2026-07-19 10:59:02 +0800

    Create metod revers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,12 @@
 package org.example;
 
+import static org.example.util.StringUtil.reverse;
+
 public class Main {
     static void main() {
     //Задание на разворот строки
-//        Input:  J@va the be$t!123
-//        Output: t@eb eht av$J!123
-        //Объявдяем стартовую строку
-        String letterInput = "1J@va the be$t!123G%";
-        //Объявляем финишную строку
-        String letterOutput;
-        //Объявляем масстив char и сразу вносим стартовую строку
-        char [] chars = letterInput.toCharArray();
-        int left = 0;
-        int right = letterInput.length()-1;
-
-        while (left < right) {
-            //Проверяем левый символ. Если не буква сразу двигаем каретку влево
-            if (!(Character.isLetter(chars[left]))) {
-                left++;
-                //Иначе проверяем что правый символ тоже является буквой
-            } else if (Character.isLetter(chars[right])) {
-                char tmp = chars[left];     // меняем местами края
-                chars[left] = chars[right];
-                chars[right] = tmp;
-                left++;                     //и двигаем обе каретки
-                right--;
-            }
-            //Проверяем правый символ. Если не буква - двигаем каретку вправо
-            if (!(Character.isLetter(chars[right]))) {
-                right--;
-            }
-        }
-            System.out.println(chars);
+        String outputLetter = "1J@va the be$t!123G%";
+        System.out.println(reverse(outputLetter));
 
         }
     }

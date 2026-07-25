5f650865edff0d0e3a41477a089dc6bfa9606c84
reshetov.uriy@gmail.com <reshetov.uriy@gmail.com>
2026-07-25 10:13:35 +0800

    add tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 import static org.example.util.StringUtil.*;
 
 public class Main {
-    static void main() {
-    //Задание на разворот строки
-        String outputLetter = "1J@va the be$t!123G%";
+    public static void main(String[] args) {
+        //Задание на разворот строки
+        String outputLetter = null;
         System.out.println(reverseLetter(outputLetter));
-        }
     }
+}
 
---
+++ src/test/java/TestReverseLetter.java
@@ -0,0 +1,105 @@
+import org.junit.jupiter.api.Test;
+
+import static org.example.util.StringUtil.reverseLetter;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestReverseLetter {
+
+    //Обычный случай
+    @Test
+    void test1() {
+        String input = "J@va the be$t!123";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "t@eb eht av$J!123";
+
+        assertEquals(expected, result);
+    }
+    //Пустая строка
+    @Test
+    void test2() {
+        String input = "";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "";
+
+        assertEquals(expected, result);
+    }
+    //Одна буква
+    @Test
+    void test3() {
+        String input = "a";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "a";
+
+        assertEquals(expected, result);
+    }
+    //Строка без букв
+    @Test
+    void test4() {
+        String input = "123 !@#";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "123 !@#";
+
+        assertEquals(expected, result);
+    }
+    //Только буквы
+    @Test
+    void test5() {
+        String input = "abcd";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "dcba";
+
+        assertEquals(expected, result);
+    }
+    //Небуквенные символы по краям и в середине
+    @Test
+    void test6() {
+        String input = "!@6J@va the&&9 beZt!123*";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "!@6t@Ze beh&&9 tavJ!123*";
+
+        assertEquals(expected, result);
+    }
+    //Регистр
+    @Test
+    void test7() {
+        String input = "J@VA the BE$T!123";
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = "T@EB eht AV$J!123";
+
+        assertEquals(expected, result);
+    }
+    //Null
+    @Test
+    void test8() {
+        String input = null;
+        // Вызываем статический метод через класс StringUtil
+        String result = reverseLetter(input);
+
+        // Вставь сюда ожидаемую строку (то, что должен вернуть твой метод)
+        String expected = null;
+
+        assertEquals(expected, result);
+    }
+
+}
